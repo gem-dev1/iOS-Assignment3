2136039bf94cd6fcffa655f91a6b19c4d75bf729
gem_dev1 <gemdevwork@gmail.com>
2024-03-29 22:42:03 -0400

    Finish all features

--- iOS-Assignment3/iOS-Assignment3/AppDelegate.swift
+++ iOS-Assignment3/iOS-Assignment3/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        allQuestions.append(a1)
-        allQuestions.append(a2)
+        //allQuestions.append(a1)
+        //allQuestions.append(a2)
         return true
     }
 
